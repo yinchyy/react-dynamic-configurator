6d54f5f4c38ee9d10fad25c1061f2ab115b018bf
Szymon Studniarz <szymek1019@gmail.com>
2022-04-03 18:09:24 +0200

    add current config storage

---
+++ src/redux/chosenConfig.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const configOptionsSlice = createSlice({
+  name: "chosenConfig",
+    initialState: {
+        activePage: 0,
+        lastPage:5,
+        selectedConfig:[]
+  },
+  reducers: {
+      nextPage: (state) => {
+          if (state.activePage < state.lastPage) {
+              state.activePage += 1;   
+          }
+      },
+      previousPage: (state) => {
+          if (state.activePage > 0) {
+              state.activePage -= 1;   
+          }
+      },
+      setConfig:(state,action) =>{
+          const elemIndex = state.selectedConfig.indexOf();
+          if (elemIndex === -1) {
+              state.selectedConfig.push(action.payload);
+          }
+          else {
+              state.selectedConfig[elemIndex] = action.payload;
+          }
+      }
+  }
+});
+
+// Action creators are generated for each case reducer function
+export const {nextPage,previousPage,setConfig} = configOptionsSlice.actions;
+
+export default configOptionsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import configOptionsReducer from './availableConfigOptions';
+import chosenConfigReducer from "./chosenConfig";
 export default configureStore({
     reducer: {
-        availableConfigOptions: configOptionsReducer
+        availableConfigOptions: configOptionsReducer,
+        chosenConfig: chosenConfigReducer
     }
 });

135795336cd96b4b268b15660d85a2329d25addf
Szymon Studniarz <szymek1019@gmail.com>
2022-04-03 18:06:01 +0200

    change rims to be objects instead of primitive values

--- src/redux/availableConfigOptions.js
+++ src/redux/availableConfigOptions.js
@@ -9,7 +9,7 @@ export const configOptionsSlice = createSlice({
       "displacement": "1998cm^3",
       "power": "180KW",
       "engine_code": "Turbo",
-      "price":"5200"
+      "price":5200
     },
     {
       "model": "b18m25i",
@@ -17,36 +17,50 @@ export const configOptionsSlice = createSlice({
       "displacement": "1763cm^3",
       "power": "130KW",
       "engine_code": "TDI",
-      "price":"6100"
+      "price":6100
     }],
     paint: [{
       name: "blue sky",
       colorCode: "#57f7ff",
-      price:"800"
+      price:800
     },
     {
       name: "red roses",
       colorCode: "#ff2e31",
-      price:"800"
+      price:800
     },
     {
       name: "yellow winegard",
       colorCode: "#ffc71d",
-      price:"850"
+      price:850
     },
     {
       name: "black cosmos",
       colorCode: "#141414",
-      price:"750"
+      price:750
     }],
-    rimSizes: [13, 14, 15, 16, 18],
-    seatTypes: [{
+    rims: [{
+      size: 16,
+      name: "alloy lite",
+      price: 800
+    },
+      {
+        size: 17,
+        name: "alloy sport",
+        price:1100
+      },
+      {
+        size: 18,
+        name: "alloy sport carbon",
+        price:1600
+    }],
+    seatType: [{
       name: "standard",
       material: "textile",
       isHeated: false,
       isVentilated: false,
       regulationWays: 3,
-      price:"900"
+      price:900
     },
     {
       name: "comfort",
@@ -54,7 +68,7 @@ export const configOptionsSlice = createSlice({
       isHeated: true,
       isVentilated: false,
       regulationWays: 5,
-      price:"2400"
+      price:2400
     },
     {
       name: "premium",
@@ -62,7 +76,7 @@ export const configOptionsSlice = createSlice({
       isHeated: true,
       isVentilated: true,
       regulationWays: 8,
-      price:"5200"
+      price:5200
     },
     {
       name: "sport",
@@ -70,39 +84,39 @@ export const configOptionsSlice = createSlice({
       isHeated: true,
       isVentilated: true,
       regulationWays: 12,
-      price:"6700"
+      price:6700
     }],
-    interiorColors: [{
+    interiorColor: [{
       name: "violet relax",
       colorCode: "#572eff",
-      price:"500"
+      price:500
     },
     {
       name: "pink happiness",
       colorCode: "#e20ef5",
-      price:"600"
+      price:600
     },
     {
       name: "yellow winegard",
       colorCode: "#ffc71d",
-      price:"650"
+      price:650
     },
     {
       name: "green garden",
       colorCode: "#12ff8a",
-      price:"660"
+      price:660
     }],
     additionalEquipment: [{
       name: "dynamic LED lights",
-      price:"2320"
+      price:2320
     },
     {
       name: "premium soundsystem",
-      price:"2198" 
+      price:2198
     },
     {
       name: "sport suspension",
-      price:"5350"
+      price:5350
     }]
   },
   reducers: {

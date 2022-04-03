b6cad6b357a6fe87a659f626add0906ede6d244d
Szymon Studniarz <szymek1019@gmail.com>
2022-04-03 21:47:48 +0200

    add paint page, use props in page generator method

--- src/App.js
+++ src/App.js
@@ -10,17 +10,15 @@ import {
 } from 'reactstrap';
 import { nextPage, previousPage } from './redux/chosenConfig';
 
-function Engine() {
-  const { engine } = useSelector((state) => state.availableConfigOptions);
-  const headers = ["Model","Fuel type","Displacement","Power","Engine code","Price"]
+function Engine({target,headers,title}) {
   return (
     <Container>
-        <header className='text-center'><h1>Engine selection</h1></header>
+      <header className='text-center'><h1>{title}</h1></header>
       <Row class="row-cols-7 d-flex flex-row">
         <Col></Col>
             {headers.map((v)=><Col><p>{v}</p></Col>)}
         </Row>
-        {engine.map((v) => {
+        {target.map((v) => {
           return (<Row className="row-cols-7 d-flex flex-row">
             <Col className='d-flex justify-content-end'><input type="radio" name="engine"/></Col>
             {Object.keys(v).map((value) => <Col>{v[value]}</Col>)}
@@ -31,11 +29,13 @@ function Engine() {
   }
   
   function Configurator() {
-  const { activePage, lastPage, selectedConfig } = useSelector((state) => state.chosenConfig);
+    const { activePage, lastPage, selectedConfig } = useSelector((state) => state.chosenConfig);
+    const { headers, engine, paint } = useSelector((state) => state.availableConfigOptions);
   const dispatch = useDispatch();
   return (
     <Container className="App-header">
-      {activePage === 0 && <Engine />}
+      {activePage === 0 && <Engine target={engine} headers={headers.engine} title="Engine selection"/>}
+      {activePage === 1 && <Engine target={paint} headers={headers.paint} title="Paint selection"/>}
         <div className='d-flex flex-row justify-content-evenly'>
         <Button outline color="primary" className='mr-2' onClick={() => dispatch(previousPage())}>&lt;</Button>
         <Button outline color="primary" className='ml-2' onClick={() => dispatch(nextPage())}>&gt;</Button>

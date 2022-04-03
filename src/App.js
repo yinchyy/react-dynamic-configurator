4f0ed1281b3e7106125bf1ba9de9cd9634ce137e
Szymon Studniarz <szymek1019@gmail.com>
2022-04-03 23:50:57 +0200

    keep config state (WIP)

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import {
   Col,
   Button
 } from 'reactstrap';
-import { nextPage, previousPage } from './redux/chosenConfig';
+import { nextPage, previousPage, setConfig } from './redux/chosenConfig';
 
-function SelectionPage({target,headers,title,inputType="radio"}) {
+function SelectionPage({ target, headers, title, selectedConfig, inputType = "radio" }) {
+  const dispatch = useDispatch();
   return (
     <Container>
       <header className='text-center'><h1>{title}</h1></header>
@@ -20,7 +21,7 @@ function SelectionPage({target,headers,title,inputType="radio"}) {
         </Row>
         {target.map((v) => {
           return (<Row className="row-cols-7 d-flex flex-row">
-            <Col className='d-flex justify-content-end'><input type={inputType} name="element"/></Col>
+            <Col className='d-flex justify-content-end'><input type={inputType} name="element" onChange={() => { dispatch(setConfig(v)) }} /></Col>
             {Object.keys(v).map((value) => <Col>{typeof(v[value])==="boolean" ? (v[value]?<div className='text-success'>Yes</div>:<div className='text-danger'>No</div>):v[value]}</Col>)}
         </Row>) 
         })}
@@ -28,7 +29,12 @@ function SelectionPage({target,headers,title,inputType="radio"}) {
     );
   }
 function FinalPage({selectedConfig}) {
-    
+  return (
+    <Container className='d-flex justify-content-center'>
+      <h1>Summary:</h1>
+      {console.log(selectedConfig)}
+      </Container>
+    );
   }
   function Configurator() {
     const { activePage, lastPage, selectedConfig } = useSelector((state) => state.chosenConfig);
@@ -36,13 +42,13 @@ function FinalPage({selectedConfig}) {
   const dispatch = useDispatch();
   return (
     <Container className="App-header">
-      {activePage === 0 && <SelectionPage target={engine} headers={headers.engine} title="Engine selection"/>}
-      {activePage === 1 && <SelectionPage target={paint} headers={headers.paint} title="Paint selection"/>}
-      {activePage === 2 && <SelectionPage target={rims} headers={headers.rims} title="Rims selection"/>}
-      {activePage === 3 && <SelectionPage target={seatType} headers={headers.seatType} title="Seat option selection"/>}
-      {activePage === 4 && <SelectionPage target={interiorColor} headers={headers.interiorColor} title="Interior color selection"/>}
-      {activePage === 5 && <SelectionPage target={additionalEquipment} headers={headers.additionalEquipment} title="Additional equipment selection" inputType='checkbox'/>}
-      {activePage === lastPage && <FinalPage />}
+      {activePage === 0 && <SelectionPage target={engine} headers={headers.engine} selectedConfig={selectedConfig} title="Engine selection"/>}
+      {activePage === 1 && <SelectionPage target={paint} headers={headers.paint} selectedConfig={selectedConfig} title="Paint selection"/>}
+      {activePage === 2 && <SelectionPage target={rims} headers={headers.rims} selectedConfig={selectedConfig} title="Rims selection"/>}
+      {activePage === 3 && <SelectionPage target={seatType} headers={headers.seatType} selectedConfig={selectedConfig} title="Seat option selection"/>}
+      {activePage === 4 && <SelectionPage target={interiorColor} headers={headers.interiorColor} selectedConfig={selectedConfig} title="Interior color selection"/>}
+      {activePage === 5 && <SelectionPage target={additionalEquipment} headers={headers.additionalEquipment} selectedConfig={selectedConfig} title="Additional equipment selection" inputType='checkbox'/>}
+      {activePage === lastPage && <FinalPage selectedConfig={selectedConfig} />}
       {activePage<lastPage?
         <div className='d-flex flex-row justify-content-evenly'>
         <Button outline color={activePage===0?"secondary":"primary"} className='mr-2' onClick={() => dispatch(previousPage())}>&lt;</Button>

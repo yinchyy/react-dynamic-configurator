28884320dc3ad739223c306bcf2938624298224d
Szymon Studniarz <szymek1019@gmail.com>
2022-04-03 21:31:33 +0200

    add Engine page, use reactstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-//import { increment } from './redux/availableConfigOptions';
-import logo from './logo.svg';
-import './App.css';
+//import { nextPage,previousPage,setConfig } from './redux/chosenConfig';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  Container,
+  Row,
+  Col,
+  Button
+} from 'reactstrap';
+import { nextPage, previousPage } from './redux/chosenConfig';
 
-function Counter() {
-  const { engine, paint } = useSelector((state) => state.availableConfigOptions);
-  const dispatch = useDispatch();
-    return (
-        <div>
-        <p>Your car engine model is: {engine[0].model} and paint color: {paint[0].name}! That costs you: {parseInt(engine[0].price) + parseInt(paint[0].price)} PLN in total.</p>
-        </div>
+function Engine() {
+  const { engine } = useSelector((state) => state.availableConfigOptions);
+  const headers = ["Model","Fuel type","Displacement","Power","Engine code","Price"]
+  return (
+    <Container>
+        <header className='text-center'><h1>Engine selection</h1></header>
+      <Row class="row-cols-7 d-flex flex-row">
+        <Col></Col>
+            {headers.map((v)=><Col><p>{v}</p></Col>)}
+        </Row>
+        {engine.map((v) => {
+          return (<Row className="row-cols-7 d-flex flex-row">
+            <Col className='d-flex justify-content-end'><input type="radio" name="engine"/></Col>
+            {Object.keys(v).map((value) => <Col>{v[value]}</Col>)}
+        </Row>) 
+        })}
+      </Container>
     );
   }
-
+  
+  function Configurator() {
+  const { activePage, lastPage, selectedConfig } = useSelector((state) => state.chosenConfig);
+  const dispatch = useDispatch();
+  return (
+    <Container className="App-header">
+      {activePage === 0 && <Engine />}
+        <div className='d-flex flex-row justify-content-evenly'>
+        <Button outline color="primary" className='mr-2' onClick={() => dispatch(previousPage())}>&lt;</Button>
+        <Button outline color="primary" className='ml-2' onClick={() => dispatch(nextPage())}>&gt;</Button>
+        </div>
+    </Container>
+    )
+  }
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Counter />
-      </header>
+      <Configurator />
     </div>
   );
 }

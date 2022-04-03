cb228f069ec970e0fa0b89e38cf31dfc852f9e17
Szymon Studniarz <szymek1019@gmail.com>
2022-04-04 00:32:46 +0200

    check radio checkbox when present in storage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import {
 } from 'reactstrap';
 import { nextPage, previousPage, setConfig } from './redux/chosenConfig';
 
-function SelectionPage({ target, headers, title, selectedConfig, inputType = "radio" }) {
+function SelectionPage({ target, targetKey, headers, title, inputType = "radio" }) {
+  const {selectedConfig} = useSelector((state) => state.chosenConfig);
   const dispatch = useDispatch();
   return (
     <Container>
@@ -21,7 +22,7 @@ function SelectionPage({ target, headers, title, selectedConfig, inputType = "ra
         </Row>
         {target.map((v) => {
           return (<Row className="row-cols-7 d-flex flex-row">
-            <Col className='d-flex justify-content-end'><input type={inputType} name="element" onChange={() => { dispatch(setConfig(v)) }} /></Col>
+            <Col className='d-flex justify-content-end'><input type={inputType} name="element" onChange={() => { dispatch(setConfig({v,targetKey}))}} checked={v===selectedConfig[targetKey]?true:false} /></Col>
             {Object.keys(v).map((value) => <Col>{typeof(v[value])==="boolean" ? (v[value]?<div className='text-success'>Yes</div>:<div className='text-danger'>No</div>):v[value]}</Col>)}
         </Row>) 
         })}
@@ -39,15 +40,16 @@ function FinalPage({selectedConfig}) {
   function Configurator() {
     const { activePage, lastPage, selectedConfig } = useSelector((state) => state.chosenConfig);
     const { headers, engine, paint, rims, seatType, interiorColor, additionalEquipment } = useSelector((state) => state.availableConfigOptions);
+    const targetKeys = useSelector((state) => Object.keys(state.availableConfigOptions));
   const dispatch = useDispatch();
   return (
     <Container className="App-header">
-      {activePage === 0 && <SelectionPage target={engine} headers={headers.engine} selectedConfig={selectedConfig} title="Engine selection"/>}
-      {activePage === 1 && <SelectionPage target={paint} headers={headers.paint} selectedConfig={selectedConfig} title="Paint selection"/>}
-      {activePage === 2 && <SelectionPage target={rims} headers={headers.rims} selectedConfig={selectedConfig} title="Rims selection"/>}
-      {activePage === 3 && <SelectionPage target={seatType} headers={headers.seatType} selectedConfig={selectedConfig} title="Seat option selection"/>}
-      {activePage === 4 && <SelectionPage target={interiorColor} headers={headers.interiorColor} selectedConfig={selectedConfig} title="Interior color selection"/>}
-      {activePage === 5 && <SelectionPage target={additionalEquipment} headers={headers.additionalEquipment} selectedConfig={selectedConfig} title="Additional equipment selection" inputType='checkbox'/>}
+      {activePage === 0 && <SelectionPage target={engine} headers={headers.engine} targetKey={targetKeys[1]} title="Engine selection"/>}
+      {activePage === 1 && <SelectionPage target={paint} headers={headers.paint} targetKey={targetKeys[2]} title="Paint selection"/>}
+      {activePage === 2 && <SelectionPage target={rims} headers={headers.rims} targetKey={targetKeys[3]} title="Rims selection"/>}
+      {activePage === 3 && <SelectionPage target={seatType} headers={headers.seatType} targetKey={targetKeys[4]} title="Seat option selection"/>}
+      {activePage === 4 && <SelectionPage target={interiorColor} headers={headers.interiorColor} targetKey={targetKeys[5]} title="Interior color selection"/>}
+      {activePage === 5 && <SelectionPage target={additionalEquipment} headers={headers.additionalEquipment} targetKey={targetKeys[6]} title="Additional equipment selection" inputType='checkbox'/>}
       {activePage === lastPage && <FinalPage selectedConfig={selectedConfig} />}
       {activePage<lastPage?
         <div className='d-flex flex-row justify-content-evenly'>

d3f2c66165c3037edbd74db10bc00316ae7280bc
Szymon Studniarz <szymek1019@gmail.com>
2022-04-04 22:34:08 +0200

    change filetype to jsx, move away unused stuff

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Engine from './components/Engines';
+
+function App() {
+  return (
+    <div className="App">
+      <Engine />
+    </div>
+  );
+}
+
+export default App;

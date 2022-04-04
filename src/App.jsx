bd04c5ef1a78440bc05ba9be1cd20172f9c9b738
Szymon Studniarz <szymek1019@gmail.com>
2022-04-05 00:02:47 +0200

    add page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useSelector } from 'react-redux';
 import Engine from './components/Engines';
+import Navigation from './components/Navigation';
 
 function App() {
+  const { activePage } = useSelector((state) => state.chosenConfig);
   return (
     <div className="App">
-      <Engine />
+      {activePage === 0 && <Engine />}
+      <Navigation />
     </div>
   );
 }

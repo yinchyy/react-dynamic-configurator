bf01d6cf07668e4021810b54d9d30132e186fbb2
Szymon Studniarz <szymek1019@gmail.com>
2022-04-04 22:31:10 +0200

    store object ID value instead of object, immutable setters add engine storage

--- src/redux/chosenConfig.js
+++ src/redux/chosenConfig.js
@@ -1,42 +1,54 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export const configOptionsSlice = createSlice({
-  name: "chosenConfig",
-    initialState: {
-        activePage: 0,
-        lastPage:6,
-        selectedConfig:{
-            engine: {},
-            paint: {},
-            rims: {},
-            seatType: {},
-            interiorColor: {},
-            additionalEquipment:[]
-        }
+  name: 'chosenConfig',
+  initialState: {
+    activePage: 0,
+    lastPage: 6,
+    engineID: 0,
+    paintID: 0,
+    rimsID: 0,
+    rimsColorID: 0,
+    seatTypeID: 0,
+    interiorColorID: 0,
+    additionalEquipmentIDs: [],
   },
   reducers: {
-      nextPage: (state) => {
-          if (state.activePage < state.lastPage) {
-              state.activePage += 1;   
-          }
-      },
-      previousPage: (state) => {
-          if (state.activePage > 0) {
-              state.activePage -= 1;   
-          }
-      },
-      setConfig:(state,action) =>{
-          if (action.payload.targetKey === 'additionalEquipment') {
-              
-          }
-          else {
-              state.selectedConfig[action.payload.targetKey] = action.payload.v;
-          }
+    nextPage: (state) => {
+      if (state.activePage < state.lastPage) {
+        return {
+          ...state,
+          activePage: state.activePage + 1,
+        };
       }
-  }
+
+      return {
+        ...state,
+      };
+    },
+    previousPage: (state) => {
+      if (state.activePage > 0) {
+        return {
+          ...state,
+          activePage: state.activePage - 1,
+        };
+      }
+      return {
+        ...state,
+      };
+    },
+    setEngine: (state, action) => ({ ...state, engineID: action.payload }),
+    setPaint: (state, action) => ({ ...state, paintID: action.payload }),
+    setRims: (state, action) => ({ ...state, rimsID: action.payload }),
+    setRimsColor: (state, action) => ({ ...state, rimsColorID: action.payload }),
+    setSeatType: (state, action) => ({ ...state, seatTypeID: action.payload }),
+    setInteriorColor: (state, action) => ({ ...state, interiorColorID: action.payload }),
+  },
 });
 
 // Action creators are generated for each case reducer function
-export const {nextPage,previousPage,setConfig} = configOptionsSlice.actions;
+export const {
+  nextPage, previousPage, setEngine,
+} = configOptionsSlice.actions;
 
 export default configOptionsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import configOptionsReducer from './availableConfigOptions';
-import chosenConfigReducer from "./chosenConfig";
+import { configureStore } from '@reduxjs/toolkit';
+import engineReducer from './availableConfigOptions/engine';
+import chosenConfigReducer from './chosenConfig';
+
 export default configureStore({
-    reducer: {
-        availableConfigOptions: configOptionsReducer,
-        chosenConfig: chosenConfigReducer
-    }
+  reducer: {
+    engine: engineReducer,
+    chosenConfig: chosenConfigReducer,
+  },
 });

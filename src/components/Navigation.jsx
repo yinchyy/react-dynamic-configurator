0a1f43cb133920bb00ef09cc462a365fca4bfe82
Szymon Studniarz <szymek1019@gmail.com>
2022-04-05 11:02:07 +0200

    mark page number, change button color / title depending on place

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button, Container } from 'reactstrap';
 import { nextPage, previousPage } from '../redux/chosenConfig';
 
 function Navigation() {
+  const { activePage, lastPage } = useSelector((state) => state.chosenConfig);
   const dispatch = useDispatch();
+  let color = 'primary';
+  let btnValue = '>';
+  if (activePage === 0) {
+    color = 'secondary';
+  } else {
+    color = 'primary';
+    if (activePage === lastPage - 1) {
+      btnValue = 'Finish';
+    } else {
+      btnValue = '>';
+    }
+  }
   return (
-    <Container>
-      <Button color="primary" outline onClick={() => { dispatch(previousPage()); }}>&lt;</Button>
-      <Button color="primary" outline onClick={() => { dispatch(nextPage()); }}>&gt;</Button>
+    <Container className="d-flex justify-content-evenly">
+      <Button color={color} outline onClick={() => { dispatch(previousPage()); }}>&lt;</Button>
+      <p>
+        {activePage + 1}
+        {' '}
+        of
+        {' '}
+        {lastPage}
+      </p>
+      <Button color="primary" outline onClick={() => { dispatch(nextPage()); }}>{btnValue}</Button>
     </Container>
   );
 }

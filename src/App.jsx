0e5af0bf9c9ead244647eac62ef500e09ae416f2
Szymon Studniarz <szymek1019@gmail.com>
2022-04-06 14:12:14 +0200

    add BodyPaint page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useSelector } from 'react-redux';
 import Engine from './components/Engines';
+import BodyPaint from './components/BodyPaint';
 import Navigation from './components/Navigation';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       {activePage === 0 && <Engine />}
+      {activePage === 1 && <BodyPaint />}
       <Navigation />
     </div>
   );

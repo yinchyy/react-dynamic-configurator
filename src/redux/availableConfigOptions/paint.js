3ac5396ae80c2296b908a7f0f03abd17c04450bb
Szymon Studniarz <szymek1019@gmail.com>
2022-04-06 14:09:12 +0200

    add ID to paints

--- src/redux/availableConfigOptions/paint.js
+++ src/redux/availableConfigOptions/paint.js
@@ -5,21 +5,25 @@ export const configOptionsSlice = createSlice({
   initialState: {
     headers: ['Paint color', 'Color code', 'Price'],
     paint: [{
+      id: 0,
       name: 'blue sky',
       colorCode: '#57f7ff',
       price: 800,
     },
     {
+      id: 1,
       name: 'red roses',
       colorCode: '#ff2e31',
       price: 800,
     },
     {
+      id: 2,
       name: 'yellow winegard',
       colorCode: '#ffc71d',
       price: 850,
     },
     {
+      id: 3,
       name: 'black cosmos',
       colorCode: '#141414',
       price: 750,

380dc6e93b11d37124baba35616f6fcafcc88846
Szymon Studniarz <szymek1019@gmail.com>
2022-04-07 11:25:37 +0200

    include seatType

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import engineReducer from './availableConfigOptions/engine';
 import paintReducer from './availableConfigOptions/paint';
 import rimsReducer from './availableConfigOptions/rims';
+import seatTypeReducer from './availableConfigOptions/seatType';
 import chosenConfigReducer from './chosenConfig';
 
 export default configureStore({
@@ -9,6 +10,7 @@ export default configureStore({
     engine: engineReducer,
     paint: paintReducer,
     rims: rimsReducer,
+    seatType: seatTypeReducer,
     chosenConfig: chosenConfigReducer,
   },
 });

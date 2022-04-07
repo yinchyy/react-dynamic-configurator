f8322983fcf3899f89aeb8b4d6851e137631b1c0
Szymon Studniarz <szymek1019@gmail.com>
2022-04-07 13:59:22 +0200

    add "configure again" functionality

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Container } from 'reactstrap';
-import { nextPage, previousPage } from '../redux/chosenConfig';
+import { nextPage, previousPage, returnToFirstPage } from '../redux/chosenConfig';
 
 function Navigation() {
   const { activePage, lastPage } = useSelector((state) => state.chosenConfig);
@@ -18,6 +18,13 @@ function Navigation() {
     } else {
       btnValue = '>';
     }
+  }
+  if (activePage === lastPage) {
+    return (
+      <Container className="d-flex justify-content-evenly">
+        <Button color="primary" onClick={() => { dispatch(returnToFirstPage()); }}>Configure again</Button>
+      </Container>
+    );
   }
   return (
     <Container className="d-flex justify-content-evenly">

216df136bd3161acd3f1bedc0f978f09066d450b
Szymon Studniarz <szymek1019@gmail.com>
2022-04-07 13:58:45 +0200

    add and export returnToFirstPage action

--- src/redux/chosenConfig.js
+++ src/redux/chosenConfig.js
@@ -37,6 +37,7 @@ export const configOptionsSlice = createSlice({
         ...state,
       };
     },
+    returnToFirstPage: (state) => ({ ...state, activePage: 0 }),
     setEngine: (state, action) => ({ ...state, engineID: action.payload }),
     setPaint: (state, action) => ({ ...state, paintID: action.payload }),
     setRims: (state, action) => ({ ...state, rimsID: action.payload }),
@@ -49,8 +50,8 @@ export const configOptionsSlice = createSlice({
 
 // Action creators are generated for each case reducer function
 export const {
-  nextPage, previousPage, setEngine, setPaint, setRimsColor, setRims, setSeatType, setInteriorColor,
-  setAddEq,
+  nextPage, previousPage, returnToFirstPage, setEngine, setPaint, setRimsColor, setRims,
+  setSeatType, setInteriorColor, setAddEq,
 } = configOptionsSlice.actions;
 
 export default configOptionsSlice.reducer;

91549095cbc2cd17c148406062869ac2171809b2
Szymon Studniarz <szymek1019@gmail.com>
2022-04-23 12:41:27 +0200

    add redux storage for tests, custom renderer with test storage provider

---
+++ src/tests/testStore.js
@@ -0,0 +1,41 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const engines = createSlice({
+  name: 'engine',
+  initialState: {
+    headers: ['Model', 'Fuel type', 'Displacement', 'Power', 'Engine code', 'Price'],
+    engine: [{
+      id: 0,
+      model: 'b20m23i',
+      fuel: 'gasoline',
+      displacement: '1998cm^3',
+      power: '180KW',
+      engine_code: 'Turbo',
+      price: 5200,
+    },
+    {
+      id: 1,
+      model: 'b18m25i',
+      fuel: 'diesel',
+      displacement: '1763cm^3',
+      power: '130KW',
+      engine_code: 'TDI',
+      price: 6100,
+    }],
+  },
+}).reducer;
+const config = createSlice({
+  name: 'chosenConfig',
+  initialState: {
+    engineID: 0,
+  },
+  reducers: {
+  },
+}).reducer;
+
+export default configureStore({
+  reducer: {
+    engine: engines,
+    chosenConfig: config,
+  },
+});

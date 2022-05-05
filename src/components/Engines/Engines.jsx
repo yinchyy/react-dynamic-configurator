1a2a485480d35088a80f9fc961ed778b32e9d497
Szymon Studniarz <szymek1019@gmail.com>
2022-05-05 18:32:30 +0200

    change html structure for header background image to fill whole width

--- src/components/Engines/Engines.jsx
+++ src/components/Engines/Engines.jsx
@@ -14,26 +14,30 @@ function Engines() {
   const { engineID } = useSelector((state) => state.chosenConfig);
   const dispatch = useDispatch();
   return (
-    <Container>
-      <header className="text-center"><h1>Engine selection</h1></header>
-      <Row className="row-cols-7 d-flex flex-row">
-        <Col />
-        {headers.map((value) => <Col>{value}</Col>)}
-      </Row>
-      {engine.map((value) => (
-        <Engine
-          id={value.id}
-          model={value.model}
-          fuel={value.fuel}
-          displacement={value.displacement}
-          power={value.power}
-          engineCode={value.engine_code}
-          price={value.price}
-          isChecked={value.id === engineID}
-          change={() => { dispatch(setEngine(value.id)); }}
-        />
-      ))}
-    </Container>
+    <div>
+      <header className="d-flex justify-content-center align-items-center">
+        <h1>Engine selection</h1>
+      </header>
+      <Container>
+        <Row className="row-cols-7 d-flex flex-row">
+          <Col />
+          {headers.map((value) => <Col>{value}</Col>)}
+        </Row>
+        {engine.map((value) => (
+          <Engine
+            id={value.id}
+            model={value.model}
+            fuel={value.fuel}
+            displacement={value.displacement}
+            power={value.power}
+            engineCode={value.engine_code}
+            price={value.price}
+            isChecked={value.id === engineID}
+            change={() => { dispatch(setEngine(value.id)); }}
+          />
+        ))}
+      </Container>
+    </div>
   );
 }
 

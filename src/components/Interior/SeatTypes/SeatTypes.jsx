19ac84729eddc0ab2b4081c15ff4cb453b85ca03
Szymon Studniarz <szymek1019@gmail.com>
2022-05-06 17:32:11 +0200

    update SeatType page

--- src/components/Interior/SeatTypes/SeatTypes.jsx
+++ src/components/Interior/SeatTypes/SeatTypes.jsx
@@ -4,14 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   Container,
   Row,
-  Col,
 } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { setSeatType } from '../../../redux/chosenConfig';
 import SeatType from './SeatType';
 
 function SeatTypes() {
-  const { headers, seatType } = useSelector((state) => state.seatType);
+  const { seatType } = useSelector((state) => state.seatType);
   const { seatTypeID } = useSelector((state) => state.chosenConfig);
   const dispatch = useDispatch();
 
@@ -24,27 +23,28 @@ function SeatTypes() {
   booleanToString.propTypes = {
     bool: PropTypes.bool,
   };
-
   return (
-    <Container>
-      <header className="text-center"><h1>Seat type selection</h1></header>
-      <Row className="row-cols-7 d-flex flex-row">
-        <Col />
-        {headers.map((value) => <Col>{value}</Col>)}
-      </Row>
-      {seatType.map((value) => (
-        <SeatType
-          name={value.name}
-          material={value.material}
-          heated={booleanToString(value.isHeated)}
-          ventilated={booleanToString(value.isVentilated)}
-          regulationWays={value.regulationWays}
-          price={value.price}
-          isChecked={value.id === seatTypeID}
-          change={() => { dispatch(setSeatType(value.id)); }}
-        />
-      ))}
-    </Container>
+    <div>
+      <header className="d-flex justify-content-center align-items-center">
+        <h1>Seat type selection</h1>
+      </header>
+      <Container>
+        <Row className="d-flex justify-content-center">
+          {seatType.map((value) => (
+            <SeatType
+              name={value.name}
+              material={value.material}
+              heated={booleanToString(value.isHeated)}
+              ventilated={booleanToString(value.isVentilated)}
+              regulationWays={value.regulationWays}
+              price={value.price}
+              isChecked={value.id === seatTypeID}
+              change={() => { dispatch(setSeatType(value.id)); }}
+            />
+          ))}
+        </Row>
+      </Container>
+    </div>
   );
 }
 
